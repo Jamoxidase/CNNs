5d9046e6c2cae3f60f18ece6227da872da2eb1a6
jamoxidase <jlarbale@ucsc.edu>
2024-04-27 17:30:09 -0700

    bulk upload

---
+++ Metagenomic Binning/Make Data/motifSampler.py
@@ -0,0 +1,105 @@
+import sys
+import itertools
+class FastAreader :
+    '''
+    Define objects to read FastA files.
+
+    instantiation:
+    thisReader = FastAreader ('testTiny.fa')
+    usage:
+    for head, seq in thisReader.readFasta():
+        print (head,seq)
+    '''
+    def __init__ (self, fname=None):
+        '''contructor: saves attribute fname '''
+        self.fname = fname
+
+    def doOpen (self):
+        ''' Handle file opens, allowing STDIN.'''
+        if self.fname is None:
+            return sys.stdin
+        else:
+            return open(self.fname)
+
+    def readFasta (self):
+        ''' Read an entire FastA record and return the sequence header/sequence'''
+        header = ''
+        sequence = ''
+
+        with self.doOpen() as fileH:
+
+            header = ''
+            sequence = ''
+
+            # skip to first fasta header
+            line = fileH.readline()
+            while not line.startswith('>') :
+                line = fileH.readline()
+            header = line[1:].rstrip()
+
+            for line in fileH:
+                if line.startswith ('>'):
+                    yield header,sequence
+                    header = line[1:].rstrip()
+                    sequence = ''
+                else :
+                    sequence += ''.join(line.rstrip().split()).upper()
+
+        yield header,sequence
+
+'''
+Sample motifs from a given sequence - call them reads. Designed for >50k read lengths
+
+for each read:
+    - count the frequency of all 4-mers to generate kmer frequency distribution profile
+    - add read name and kmer frequency profile to CSV file
+
+'''
+
+# for very large reads, we can probably use smaller samples
+
+class ClusterK:
+    def __init__(self, path):
+        
+        thisReader=FastAreader(path)
+        self.heads = []
+        self.seqs = []
+
+        for head, seq in thisReader.readFasta():
+            self.heads.append(head)
+            self.seqs.append(seq)
+
+        # code to generate all permutations of DNA 4-mers
+        self.fourMers = [''.join(p) for p in itertools.product('ACGT', repeat=4)]
+        
+
+    def sampleMotifs(self, outPath):
+        '''
+        We have self.fourMers, a list of all possible 4-mers. We also have a list self.heads, and a list self.seqs.
+        This method will write to file 'testProfiles.csv' the read name and the kmer frequency profile. The CSV will
+        have a column for the read name, and a column for each 4-mer. The values will be the read name and frequency 
+        of each 4-mer (with overlap). EG. if the read is "AGTCT", then the 4-mers present are "AGTC", "GTCT".
+
+        for each read, determine the count of all 4-mers.
+        '''
+        with open('testProfiles.csv', 'w') as file:
+            file.write('Read Name,')
+            file.write(','.join(self.fourMers))
+            file.write('\n')
+            for i in range(len(self.heads)):
+                read_name = self.heads[i]
+                kmer_freq = [str(self.seqs[i].count(kmer) / (len(self.seqs[i])-3)) for kmer in self.fourMers]
+                file.write(read_name + ',')
+                file.write(','.join(kmer_freq))
+                file.write('\n')
+
+
+
+def main():
+    path = '/Users/jlarbale/Desktop/clusterKdata/codeBase/testReads.txt'
+    clusterK = ClusterK(path)
+    clusterK.sampleMotifs('')
+
+
+if __name__ == '__main__':
+    main()
---
+++ capsuleNetwork/readme.md
@@ -0,0 +1,33 @@
+# Capsule Network for MNIST Digit Classification
+
+This notebook contains a PyTorch implementation of a Capsule Network (CapsNet) trained to classify handwritten digits from the MNIST dataset. Capsule Networks, introduced by Hinton et al. in "Dynamic Routing Between Capsules" (2017), offer an interesting alternative to traditional convolutional neural networks (CNNs) by seeking to capture hierarchical relationships between features with a parse-tree like approach designed to capture hierarchical features in high-level capsules. Laue et al. in "Why Capsule Neural Networks Do Not Scale: Challenging the Dynamic Parse-Tree Assumption" (2023) present findings that highlight some limitations of CapsNet arcitecture and a lack of scalibility, although I do not think that this impedes on the interesting thought experement that is CapsNet. I had a good time learning about this arcitecture as it is based on a "natural" approach to feature extraction, although there is some residual numerical instability in my current implimentation. Nonetheless it achievs 99%+ accuracy over the full MNIST test set, and this was my first crack at MNIST which has been pretty cool to explore. 
+
+## Overview
+
+This notebook includes the following components:
+
+- `CapsNet.py`: Contains the implementation of the Capsule Network model (`CapsNet` class) and its associated components such as convolutional layers (`ConvLayer`), primary capsules (`PrimaryCaps`), digit capsules (`DigitCaps`), and the decoder (`Decoder`).
+
+- `train.py`: Implements the training loop for the Capsule Network model. It iterates over epochs, updates model parameters, and evaluates performance.
+
+- `evaluate.py`: Provides utility functions for evaluating the model's performance on test data.
+
+- `inference.ipynb`: A Jupyter notebook where users can upload their own handwritten digit images for inference using the trained Capsule Network model.
+
+## Usage
+
+1. **Training**: Run `train.py` to train the Capsule Network model. You can customize training parameters such as batch size, optimizer/learning rate, and number of epochs in the script.
+
+```bash
+python train.py
+```
+
+2. **Inference**: Use the `inference.ipynb` notebook to upload your own handwritten digit images and run inference using the trained model. Follow the instructions provided in the notebook for step-by-step guidance.
+
+## Known Issues
+
+There is a known numerical instability issue causing the model to degrade rapidly after excessive epochs. Efforts are underway to address this issue and improve model stability.
+
+## Acknowledgments
+
+Special thanks to Hinton et al. for their pioneering work on Capsule Networks and the PyTorch community for providing robust deep learning tools and frameworks. And to [This User](https://github.com/cezannec/capsule_net_pytorch/blob/master/Capsule_Network.ipynb) for their great overview of routing capsule networks.
